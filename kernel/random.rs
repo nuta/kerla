b80b95fefab3ddfcec91f3c6d28101ef36d02571
Seiya Nuta <nuta@seiya.me>
2021-04-06 22:02:38 +0900

    Add a TODO comment

--- kernel/random.rs
+++ kernel/random.rs
@@ -3,6 +3,8 @@ use crate::user_buffer::UserBufferMut;
 use x86::random::rdrand_slice;
 
 pub fn read_secure_random(mut buf: UserBufferMut<'_>) -> Result<usize> {
+    // TODO: Implement arch-agnostic CRNG which does not fully depends on RDRAND.
+
     buf.write_with(|slice| {
         let valid = unsafe { rdrand_slice(slice) };
         if valid {
