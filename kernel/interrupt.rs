b213c965d3127a35b429da62fa9d134304107fbf
Sergey Bargamon <sergey@bargamon.ru>
2021-10-31 11:23:41 +0100

    ISSUE-29: Switch IRQ handler's map from BTreeMap to array

--- kernel/interrupt.rs
+++ kernel/interrupt.rs
@@ -5,19 +5,33 @@ use crate::{
     net::process_packets,
 };
 use alloc::boxed::Box;
-use alloc::collections::BTreeMap;
 
-// TODO: Use a simple array for faster access.
-static IRQ_HANDLERS: SpinLock<BTreeMap<u8, Box<dyn FnMut() + Send + Sync>>> =
-    SpinLock::new(BTreeMap::new());
+static IRQ_HANDLERS: SpinLock<[Option<Box<dyn FnMut() + Send + Sync>>; 255]> = SpinLock::new([
+    None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+    None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+    None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+    None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+    None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+    None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+    None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+    None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+    None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+    None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+    None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+    None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+    None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+    None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+    None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+    None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+]);
 
 pub fn attach_irq<F: FnMut() + Send + Sync + 'static>(irq: u8, f: F) {
-    IRQ_HANDLERS.lock().insert(irq, Box::new(f));
+    IRQ_HANDLERS.lock()[irq as usize] = Some(Box::new(f));
     enable_irq(irq);
 }
 
 pub fn handle_irq(irq: u8) {
-    if let Some(handler) = IRQ_HANDLERS.lock().get_mut(&irq) {
+    if let Some(handler) = &mut IRQ_HANDLERS.lock()[irq as usize] {
         (*handler)();
         process_packets();
     }
