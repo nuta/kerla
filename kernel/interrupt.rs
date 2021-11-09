8f2fce6273d4f8a713344e33bcab42f031c540a6
Serhiy Barhamon <sergey@bargamon.ru>
2021-11-09 10:33:44 +0100

    Panic on irq_handler overwrite (#53)
    * Panic on irq_handler overwrite
    
    * using BitMap to track attached IRQ numbers

--- kernel/interrupt.rs
+++ kernel/interrupt.rs
@@ -3,6 +3,7 @@
 use crate::arch::{enable_irq, SpinLock};
 use alloc::boxed::Box;
 use core::mem::MaybeUninit;
+use kerla_utils::bitmap::BitMap;
 
 fn empty_irq_handler() {}
 
@@ -10,6 +11,7 @@ type IrqHandler = dyn FnMut() + Send + Sync;
 const UNINITIALIZED_IRQ_HANDLER: MaybeUninit<Box<IrqHandler>> = MaybeUninit::uninit();
 static IRQ_HANDLERS: SpinLock<[MaybeUninit<Box<IrqHandler>>; 256]> =
     SpinLock::new([UNINITIALIZED_IRQ_HANDLER; 256]);
+static ATTACHED_IRQS: SpinLock<BitMap<32 /* = 256 / 8 */>> = SpinLock::new(BitMap::zeroed());
 
 pub fn init() {
     let mut handlers = IRQ_HANDLERS.lock();
@@ -19,8 +21,16 @@ pub fn init() {
 }
 
 pub fn attach_irq<F: FnMut() + Send + Sync + 'static>(irq: u8, f: F) {
-    IRQ_HANDLERS.lock()[irq as usize].write(Box::new(f));
-    enable_irq(irq);
+    let mut attached_irq_map = ATTACHED_IRQS.lock();
+    match attached_irq_map.get(irq as usize) {
+        Some(true) => panic!("handler for IRQ #{} is already attached", irq),
+        Some(false) => {
+            attached_irq_map.set(irq as usize);
+            IRQ_HANDLERS.lock()[irq as usize].write(Box::new(f));
+            enable_irq(irq);
+        }
+        None => panic!("IRQ #{} is out of bound", irq),
+    }
 }
 
 pub fn handle_irq(irq: u8) {
