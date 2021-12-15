b5269770a1b561dcb104e6f77173d4c459ccf2b9
Seiya Nuta <nuta@seiya.me>
2021-12-15 21:18:10 +0900

    Use yapf for formatting python scripts (#125)

--- tools/inspect-init-in-docker-image.py
+++ tools/inspect-init-in-docker-image.py
@@ -8,9 +8,13 @@ import json
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Determines the init argv specified in a Docker image (i.e. CMD).")
-    parser.add_argument("image",
-                        help="The docker image name (e.g. python:slim or docker.io/library/python:alpine).")
+        description=
+        "Determines the init argv specified in a Docker image (i.e. CMD).")
+    parser.add_argument(
+        "image",
+        help=
+        "The docker image name (e.g. python:slim or docker.io/library/python:alpine)."
+    )
     args = parser.parse_args()
 
     try:
@@ -18,7 +22,8 @@ def main():
             ["docker", "image", "inspect", args.image])
     except subprocess.CalledProcessError as e:
         sys.exit(
-            f"{e.stdout.decode('utf-8', 'backslashreplace')}\n\nError: failed to inspect {args.image}")
+            f"{e.stdout.decode('utf-8', 'backslashreplace')}\n\nError: failed to inspect {args.image}"
+        )
 
     data = json.loads(stdout)[0]["Config"]
     print(shlex.join(data["Cmd"]))
