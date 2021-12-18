7e54ab964afac70d43f468d76674e88afffc3fab
Seiya Nuta <nuta@seiya.me>
2021-12-18 17:50:42 +0900

    downcast: Take a reference instead of the ownership (#130)

--- libs/kerla_utils/downcast.rs
+++ libs/kerla_utils/downcast.rs
@@ -2,19 +2,19 @@ use alloc::sync::Arc;
 use core::any::Any;
 
 pub trait Downcastable: Any + Send + Sync {
-    fn as_any(self: Arc<Self>) -> Arc<dyn Any + Send + Sync>;
+    fn as_any(&self) -> &dyn Any;
 }
 
 impl<T: Any + Send + Sync> Downcastable for T {
-    fn as_any(self: Arc<Self>) -> Arc<dyn Any + Send + Sync> {
+    fn as_any(&self) -> &dyn Any {
         self
     }
 }
 
-pub fn downcast<S, T>(arc: &Arc<S>) -> Option<Arc<T>>
+pub fn downcast<S, T>(arc: &Arc<S>) -> Option<&Arc<T>>
 where
     S: Downcastable + ?Sized,
     T: Send + Sync + 'static,
 {
-    arc.clone().as_any().downcast::<T>().ok()
+    arc.as_any().downcast_ref::<Arc<T>>()
 }
