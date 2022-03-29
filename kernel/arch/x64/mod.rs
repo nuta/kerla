758fe047919ce922d8c091b38c72d5378fd43891
Seiya Nuta <nuta@seiya.me>
2022-03-29 14:12:16 +0000

    Update

--- kernel/arch/x64/mod.rs
+++ kernel/arch/x64/mod.rs
@@ -1,13 +1,13 @@
 use core::arch::global_asm;
 
-use kerla_runtime::{address::UserVAddr, arch::PAGE_SIZE};
+use kerla_runtime::address::UserVAddr;
 
 global_asm!(include_str!("usermode.S"));
 
 mod arch_prctl;
 mod process;
 
-pub const KERNEL_STACK_SIZE: usize = PAGE_SIZE * 256;
+pub const KERNEL_STACK_SIZE: usize = 1024 * 16;
 pub const USER_VALLOC_END: UserVAddr = unsafe { UserVAddr::new_unchecked(0x0000_0fff_0000_0000) };
 pub const USER_VALLOC_BASE: UserVAddr = unsafe { UserVAddr::new_unchecked(0x0000_000a_0000_0000) };
 pub const USER_STACK_TOP: UserVAddr = USER_VALLOC_BASE;
